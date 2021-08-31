ef5698e8da72550733200fb0a9d7c880d3f7fd35
devTawhid <thefreelancertawhid@gmail.com>
2021-08-31 16:53:35 +0600

    day-6 done

---
+++ day-6/js/app.js
@@ -0,0 +1,43 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+// fetch(endpoint).then(data =>{
+//     // console.log(data);
+//     return data.json();
+// }).then(data => console.log(data, 'recieved'));
+
+const cities = [];
+
+fetch(endpoint)
+              .then(data => data.json())
+              .then(data => cities.push(...data))
+              .catch(err => console.error(err.msg));
+
+function findMatch(wordToFind, cities){
+    return cities.filter(places => {
+        let regex = new RegExp(wordToFind, 'gi');
+        return places.city.match(regex) || places.state.match(regex);
+    })
+}
+
+let input = document.querySelector('.search');
+let suggestions = document.querySelector('.suggestions');
+
+function display(){
+    let value = findMatch(this.value, cities);
+    console.log(value);
+
+    let html = [...value].map(place => {
+        let regex = new RegExp(this.value, 'gi');
+        let cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        let stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `
+            <li>
+                <span class="name">${cityName}, ${stateName}</span>
+                <span class="population">${place.population}</span>
+            </li>`;
+    }).join('');
+
+    suggestions.innerHTML = html;
+}
+
+"change keypress".split(" ").forEach(e => input.addEventListener(e, display));
