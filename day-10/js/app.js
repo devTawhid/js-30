afb1520ffe103dfee75af54937659ac292509562
devTawhid <thefreelancertawhid@gmail.com>
2021-09-05 22:03:22 +0600

    day-10 done

---
+++ day-10/js/app.js
@@ -0,0 +1,21 @@
+const checkbox = document.querySelectorAll('input[type="checkbox"]');
+
+let lastChecked = false;
+let inBetween = false;
+
+function handleCheck(e){
+    if(e.shiftKey && this.checked && lastChecked.checked === true) {
+        checkbox.forEach(input => {
+            if(input.checked || lastChecked === this){
+                inBetween = !inBetween;
+            }
+            if(inBetween){
+                input.checked = true;
+            }
+        });
+    }
+    
+    lastChecked = this;
+}
+
+[...checkbox].forEach(input => input.addEventListener('click', handleCheck) );
