0869ca424655a28d7be56830d039890ff0b68ad5
devTawhid <thefreelancertawhid@gmail.com>
2021-09-21 14:47:59 +0600

    day 17 done

---
+++ day-17/js/app.js
@@ -0,0 +1,10 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const band = document.querySelector('#bands');
+
+bands.sort();
+let html = bands.map(val => {
+    return `<li> ${val} </li>`;
+}).join('');
+
+band.innerHTML = html;
